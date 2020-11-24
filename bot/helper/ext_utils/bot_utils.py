23a46a47b660a101704c8b91aaf438c017da839d
Madhavreddy672 <71065430+Madhavreddy672@users.noreply.github.com>
2020-11-24 07:06:19 +0000

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -13,17 +13,17 @@ URL_REGEX = r"(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+"
 
 
 class MirrorStatus:
-    STATUS_UPLOADING = "Uploading"
-    STATUS_DOWNLOADING = "Downloading"
+    STATUS_UPLOADING = "Uploading...📤"
+    STATUS_DOWNLOADING = "Downloading 📥"
     STATUS_WAITING = "Queued"
-    STATUS_FAILED = "Failed. Cleaning download"
-    STATUS_CANCELLED = "Cancelled"
-    STATUS_ARCHIVING = "Archiving"
-    STATUS_EXTRACTING = "Extracting"
+    STATUS_FAILED = "Failed 🚫.  Cleaning download"
+    STATUS_CANCELLED = "Cancelled ❎"
+    STATUS_ARCHIVING = "Archiving 🔐"
+    STATUS_EXTRACTING = "Extracting 📂"
 
 
 PROGRESS_MAX_SIZE = 100 // 8
-PROGRESS_INCOMPLETE = ['▏', '▎', '▍', '▌', '▋', '▊', '▉']
+PROGRESS_INCOMPLETE = ['⚫', '⚫', '⚫', '⚫', '⚫', '⚫', '⚫']
 
 SIZE_UNITS = ['B', 'KB', 'MB', 'GB', 'TB', 'PB']
 
